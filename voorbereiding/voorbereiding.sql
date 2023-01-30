4716917449366775caca3cfff812b7233519a91e
Daniël <59751559+daniel1890@users.noreply.github.com>
2023-01-30 02:37:34 +0100

    Update voorbereiding.sql

--- voorbereiding/voorbereiding.sql
+++ voorbereiding/voorbereiding.sql
@@ -37,3 +37,25 @@ SELECT B.stuknr FROM
 Bezettingsregel B
 WHERE B.instrumentnaam = 'saxofoon'
 )
+
+-- 5.	Bij welke jazzstukken worden twee of meer verschillende instrumenten gebruikt?
+
+SELECT *
+FROM Stuk S
+WHERE S.genrenaam = 'jazz'
+AND S.stuknr IN
+(
+SELECT COUNT(*) AS AantalInstrumenten FROM
+Bezettingsregel B
+GROUP BY B.stuknr
+HAVING COUNT(*) > 0
+)
+
+-- 6.	Geef het aantal originele muziekstukken per componist. Ook componisten met nul originele stukken dienen te worden getoond.
+
+SELECT C.naam, C.componistId, COUNT(*) AS AantalMuziekstukken FROM
+Componist C
+LEFT OUTER JOIN
+Stuk S
+ON C.componistId = S.componistId
+GROUP BY C.naam, C.componistId
